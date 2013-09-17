ab4cf17f6e2e9fbe4fcb7b02a65c8646bbfd6f69
Thomas Levine <occurrence@thomaslevine.com>
2013-09-17 19:42:10 -0400

    oeu

--- numbers.py
+++ numbers.py
@@ -8,6 +8,22 @@ phone_numbers = [
     '8801292432437',
 ]
 
+class Belief:
+    def __init__(self):
+        self.d = {}
+
+    def add(self, phone_number):
+        for i in range(1, len(phone_number) - 1):
+            self.d[phone_number[:i]] = mc.DiscreteUniform('_' + pn, 0, 9)
+
+    def select
+
+    @mc.deterministic
+    def lambda_(tau=tau, lambda_1=lambda_1, lambda_2=lambda_2):
+        out = np.zeros(n_data_points)
+        out[tau:] = lambda_2  # lambda after tau is lambda1
+        return out"
+
 def train(belief, phone_number):
     for pn in partials(phone_number):
         if pn not in belief:
