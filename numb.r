08e4661d36ff263ab387f002c12b266443c45ba9
Thomas Levine <occurrence@thomaslevine.com>
2013-09-17 09:44:31 -0400

    random phone numbers

--- numb.r
+++ numb.r
@@ -1,3 +1,5 @@
+library(plyr)
+
 phone.us.srs <- function() {
   paste(c('001', sample(0:9, replace = TRUE)), collapse = '')
 }
@@ -5,8 +7,12 @@ phone.us.srs <- function() {
 #' param phone numbers (character)
 #' output data frame
 as.data.frame.phone.number <- function(phone.numbers) {
-  df <- dply(strsplit(phone.numbers, split = ''))
-  names(df) <- c('country','d1','d2','d3','d4','d5','d6','d7','d8','d9','d10','response')
+  df <- ldply(strsplit(phone.numbers, split = ''))
+  df$V1 <- paste0(df$V1, df$V2, df$V3)
+  df$V2 <- df$V3 <- NULL
+  names(df) <- c('country','d1','d2','d3','d4','d5','d6','d7','d8','d9','d10')
+  df$response <- as.logical(rbinom(nrow(df), 1, 0.3))
+  df
 }
 
-replicate(100, phone.us.srs())
+numbers <- as.data.frame.phone.number(replicate(100, phone.us.srs()))
