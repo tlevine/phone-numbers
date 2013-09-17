d10a9e2225d14dc4d8be7a78936429c197339d85
Thomas Levine <occurrence@thomaslevine.com>
2013-09-17 09:37:57 -0400

    oeu

--- numb.r
+++ numb.r
@@ -1,3 +1,10 @@
 phone.us.srs <- function() {
   paste(c('001', sample(0:9, replace = TRUE)), collapse = '')
 }
+
+#' param phone numbers (character)
+#' output data frame
+as.data.frame.phone.number <- function(phone.numbers) {
+}
+
+replicate(100, phone.us.srs())
