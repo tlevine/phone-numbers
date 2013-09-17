150402bcd38561adc3df30fda578bfca4eff248d
Thomas Levine <occurrence@thomaslevine.com>
2013-09-17 18:37:46 -0400

    oeu

--- numbers.py
+++ numbers.py
@@ -1,6 +1,18 @@
+import pymc as mc
+
 phone_numbers = [
-    ('880', '1292432439'),
-    ('880', '1292432694'),
-    ('880', '1292432275'),
-    ('880', '1292432437'),
+    '8801292432439',
+    '8801292432694',
+    '8801292432275',
+    '8801292432437',
 ]
+
+def partial_numbers(phone_numbers):
+    out = set()
+    for phone_number in phone_numbers:
+        for i in range(0, len(phone_number)):
+            out.add(phone_number[:i])
+    return out
+
+pns = partial_numbers(phone_numbers)
+distributions = {pn: mc.DiscreteUniform(pn, 0, 9) for pn in pns}
