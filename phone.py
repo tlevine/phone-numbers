b260aabdb72cf7ab4470ffdaf2099866c8cce96c
Thomas Levine <occurrence@thomaslevine.com>
2013-09-21 20:16:16 +0000

    doctest

--- phone.py
+++ phone.py
@@ -11,5 +11,21 @@ def observe_phone_number(observations, number):
         observations[number[:i]].update(number[i])
     return observations
 
-def phone_digit(observations):
+def smooth(counter, keys = map(str, range(10))):
+    '''
+    Apply +1 smoothing to a counter.
+
+    >>> smooth(Counter('bcb'), 'abcde')
+    Counter('abcdebcb')
+
+    '''
+    smoothed_counter = Counter(counter)
+    smoothed_counter.update(smoothed_counter.keys()
+    return smoothed_counter
+
+def weight(counts):
+    'Turn counts into weights.'
+
+def choose_next_digit(observations, partial_number):
+    weights = smooth(observations[partial_number])
     pymc.Categorical
