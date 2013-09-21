60328f9115acfc20d8ca08c190d7beb3f2f4841d
Thomas Levine <occurrence@thomaslevine.com>
2013-09-21 19:57:14 +0000

    pretty version

---
+++ n.py
@@ -0,0 +1,16 @@
+from collections import defaultdict, Counter
+
+import pymc
+
+
+def observations():
+    return defaultdict(Counter)
+
+def observe_phone_number(observations, number):
+    'Add the phone number stuff to the observations dict.'
+    for i in range(len(number)):
+        observations[number[:i]].update(number[i])
+    return observations
+
+def phone_digit(observations):
+    pymc.Categorical
